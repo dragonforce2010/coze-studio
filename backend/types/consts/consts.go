8e27f5c8f5fe553b0125493d6d519808e0959d40
lvgj-stack <765171999@qq.com>
2025-08-12 21:26:14 +0800

    feat(model): support volcengine-maas

--- backend/types/consts/consts.go
+++ backend/types/consts/consts.go
@@ -95,6 +95,15 @@ const (
 
 	ParserType        = "PARSER_TYPE"
 	PPStructureAPIURL = "PADDLEOCR_STRUCTURE_API_URL"
+
+	ModelProvider               = "MODEL_PROVIDER"
+	ModelProviderVolcengineMAAS = "volcengine_maas"
+	ModelProviderStatic         = "static"
+
+	VolcengineMAASAccessKey = "VOLCENGINE_MAAS_ACCESS_KEY"
+	VolcengineMAASSecretKey = "VOLCENGINE_MAAS_SECRET_KEY"
+	VolcengineMAASRegion    = "VOLCENGINE_MAAS_REGION"
+	VolcengineMAASAPIKey    = "VOLCENGINE_MAAS_API_KEY"
 )
 
 const (
